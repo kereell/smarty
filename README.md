ac9dd896344d46ccf02f50ac2c6ebed328d19a53
Kereell <Kereell@altair>
2012-09-25 22:18:38 +0300

    Changing .htaccess and config to move from dev
    Change-Id: I40fede4d86a0e5d1fd49aba6201434944f0801fa

--- README.md
+++
@@ -1,4 +0,0 @@
-smarty
-======
-
-Test Task
--- etc/config.php
+++ etc/config.php
@@ -7,9 +7,9 @@ define('DEVELOPER_MODE', TRUE);
 define('SMARTY_DIR', ROOT.DS.'libraries'.DS.'smarty'.DS);
 
 /** CSS IMG JS **/
-define('CSS_DIR', DS.'dev'.DS.'css'.DS);
-define('IMG_DIR', DS.'dev'.DS.'img'.DS);
-define('JS_DIR', DS.'dev'.DS.'js'.DS);
+define('CSS_DIR', DS.'smarty'.DS.'css'.DS);
+define('IMG_DIR', DS.'smarty'.DS.'img'.DS);
+define('JS_DIR', DS.'smarty'.DS.'js'.DS);
 
 /** CONFIGURATION DB **/
 define('DB_HOST', '');
